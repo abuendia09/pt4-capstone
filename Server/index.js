0b3a19305352e756d5c0b5b8eb25efae712027e1
Adrie <adriebuendia@icloud.com>
2022-07-23 23:56:34 -0400

    finished project

--- Server/index.js
+++ Server/index.js
@@ -15,19 +15,6 @@ const {
 
 app.use(express.static(path.join(__dirname, "../Public")));
 
-// app.get("/", function(req, res){
-//     res.sendFile(path.join(__dirname, "../Public/index.html"));
-// })
-// app.get("/", function(req, res){
-//     res.sendFile(path.join(__dirname, "../Public/add.html"));
-// })
-// app.get("/styles", function(req, res){
-//     res.sendFile(path.join(__dirname, "../Public/index.css"));
-// })
-// app.get("/styles", function(req, res){
-//     res.sendFile(path.join(__dirname, "../Public/add.css"));
-// })
-
 app.get(`/api/songs`, getSongs);
 app.post(`/api/songs`, createSong);
 app.delete(`/api/songs/:id`, deleteSong);
